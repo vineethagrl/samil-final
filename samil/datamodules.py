659c34aca9491433cc68d494dd419edac92b7a97
vineethagrl <116576584+vineethagrl@users.noreply.github.com>
2025-11-30 18:20:02 -0600

    Create datamodules.py

---
+++ samil/datamodules.py
@@ -0,0 +1,42 @@
+import os, glob, torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+
+class EchoStudyBags(Dataset):
+    def __init__(self, root, split="train", img_size=112):
+        self.root = root
+        self.split = split
+        self.studies = sorted(glob.glob(os.path.join(root, split, "study_*")))
+        self.labels = self._load_labels(os.path.join(root, f"{split}_labels.csv"))
+        self.tf = transforms.Compose([transforms.Resize((img_size, img_size)), transforms.ToTensor()])
+
+    def _load_labels(self, csv_path):
+        mapping = {}
+        if os.path.exists(csv_path):
+            with open(csv_path) as f:
+                for line in f:
+                    sid, y = line.strip().split(",")
+                    mapping[sid] = int(y)
+        return mapping
+
+    def __len__(self): return len(self.studies)
+
+    def __getitem__(self, idx):
+        sdir = self.studies[idx]
+        sid = os.path.basename(sdir)
+        imgs = []
+        for p in sorted(glob.glob(os.path.join(sdir, "*.png"))):
+            img = Image.open(p).convert("L")
+            imgs.append(self.tf(img))
+        bag = torch.stack(imgs)  # [K,1,H,W]
+        y = self.labels.get(sid, 0)
+        return bag, torch.tensor(y), sid
+
+def make_loaders(root, img_size, bs=1):
+    train = EchoStudyBags(root, "train", img_size)
+    val   = EchoStudyBags(root, "val", img_size)
+    test  = EchoStudyBags(root, "test", img_size)
+    return (DataLoader(train, batch_size=bs, shuffle=True, num_workers=2),
+            DataLoader(val,   batch_size=1, shuffle=False, num_workers=2),
+            DataLoader(test,  batch_size=1, shuffle=False, num_workers=2))
